c2a9e1f07ef2817cb5138aabe5c78cdfe3af616f
willmcdeezy <will@yatori.io>
2026-01-11 16:37:39 -0600

    Initial commit

---
+++ src/components/my-form.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const name = ref('');
+
+const handleSubmit = () => {
+    console.log(name.value);
+}
+</script>
+
+<template>
+    <form @submit.prevent="handleSubmit">
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="name" />
+    </form>
+</template>
